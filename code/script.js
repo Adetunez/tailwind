d810aac2737cb5f978423ef40b50a9be5d3edcdf
O-tega <tega.myview@gmail.com>
2022-05-03 13:12:36 +0100

    nav bar correction

--- code/script.js
+++ code/script.js
@@ -1,8 +1,15 @@
 function Menu(e) {
-    let list = document.querySelector('ul');
+    let list = document.querySelector('.newul');
+    let menu2 = document.querySelector('.menu2');
     e.name === 'menu' ? (e.name = "close", 
     list.classList.add('top-[80px]'), 
+    menu2.classList.add('mt-52'),
     list.classList.add('opacity-100')) : (e.name = "menu", 
     list.classList.remove('top-[80px]'), 
+    menu2.classList.remove('mt-52'),
     list.classList.remove('opacity-100'))
+
+    // e.name === 'menu2' ? (e.name = "close",
+    // menu2.classList.add('pb-40')): (e.name = 'menu2',
+    // menu2.classList.remove('pb-40'))
 }
